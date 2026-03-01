0fbcf7c5fde6312db3dfb1ebc2282ba5d27b6a84
billaliq <iqkhan768@gmail.com>
2026-03-01 22:42:00 +0500

    Add project files.

---
+++ SearchLibrary/Algorithms/InterpolationSearch.cs
@@ -0,0 +1,32 @@
+﻿namespace SearchLibrary.Algorithms
+{
+    public class InterpolationSearch
+    {
+        public int Search(int key, int[] elemArray)
+        {
+            if (elemArray == null || elemArray.Length == 0)
+                return -1;
+
+            int low = 0;
+            int high = elemArray.Length - 1;
+
+            while (low <= high && key >= elemArray[low]
+                               && key <= elemArray[high])
+            {
+                if (elemArray[high] == elemArray[low])
+                {
+                    if (elemArray[low] == key) return low;
+                    else return -1;
+                }
+
+                int pos = low + ((key - elemArray[low]) * (high - low))
+                               / (elemArray[high] - elemArray[low]);
+
+                if (elemArray[pos] == key) return pos;
+                if (elemArray[pos] < key) low = pos + 1;
+                else high = pos - 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ SearchLibrary/Algorithms/LinearSearch.cs
@@ -0,0 +1,18 @@
+﻿namespace SearchLibrary.Algorithms
+{
+    public class LinearSearch
+    {
+        public int Search(int key, int[] elemArray)
+        {
+            if (elemArray == null || elemArray.Length == 0)
+                return -1;
+
+            for (int i = 0; i < elemArray.Length; i++)
+            {
+                if (elemArray[i] == key)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
